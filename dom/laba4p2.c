b6823d5114d0b68408866bc0b9296d130ae6468a
RedWolfs <dpatelczyk@sigma.ug.edu.pl>
2013-11-11 08:10:59 +0100

    program sprawdza czy liczba jest dodatnia czy ujemna

---
+++ dom/laba4p2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main(){
+int  a;
+printf("Podaj liczbe\n");
+scanf("%d",&a);
+if (a<0-1){
+printf("liczna jest ujemna\n");
+}
+else if(a>0){
+printf("liczba jest dodatnnia\n");
+}
+return 0;
+}
+
