0f55d469bd9fbd714315e18dc0f69565c03b0253
RedWolfs <dpatelczyk@sigma.ug.edu.pl>
2013-11-11 09:01:15 +0100

    program sprawdza jaka liczba jest większa ,mniejsza,równa

---
+++ dom/laba4p5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main(){
+int  a ,b;
+
+printf("podaj liczbe\n");
+scanf("%d%d",&a,&b);
+	
+	if(a>b)
+	printf(" a jest większe od b\n");
+	
+	else if (a==b)
+	printf("liczby są równe\n");
+
+	else 
+	printf(" a jest mniejsze od b \n");
+	
+
+return 0;
+} 
