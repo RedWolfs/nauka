be390297a606b91ac2b9b4da46e4b4bc5ffe60f5
RedWolfs <dpatelczyk@sigma.ug.edu.pl>
2013-11-11 08:42:40 +0100

    program sprawdza czy liczby są równe czy nie

---
+++ dom/laba4p4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main(){
+int  a1 ,a2;
+printf("podaj liczbe\n");
+scanf("%d%d",&a1,&a2);
+if(a1==a2)
+printf("liczby są równe\n");
+
+else 
+printf("liczby nie są równe\n");
+
+return 0;
+} 
