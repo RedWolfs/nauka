831e2af7750be9723269b859bd82e9055e0f64df
RedWolfs <dpatelczyk@sigma.ug.edu.pl>
2013-11-12 20:02:35 +0100

    program który odpowiada czy podana liczba z zakresu od 1 do 10000 jest parzysta

---
+++ dom/laba3/4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+ main(){
+int a=0;
+for(a=0;a<=10000;a=a+2){	//;a=a+2
+printf("Podaj liczbe:");scanf("%d",&a);
+if(a%2==0)
+printf("liczna jest parzysta\n");
+
+}
+
+return 0;
+}
