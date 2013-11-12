500d32a830df5574c8b3146fee5c9a218e184ec3
RedWolfs <dpatelczyk@sigma.ug.edu.pl>
2013-11-12 19:58:06 +0100

    program oblicza pole prostokąta

---
+++ dom/laba3/1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+ main(){
+ int b,a,wynik ;
+printf("program oblicza pole prostokata\n");
+printf("podaj a:");scanf("%d",&a);
+printf("podaj b:");scanf("%d",&b);
+
+
+wynik=a*b;
+printf("pole wynosi%d\n",wynik);
+
+    
+  return 0;
+}
