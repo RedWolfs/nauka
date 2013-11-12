035c296ad29742958730636472589cfe8d64dfd6
RedWolfs <dpatelczyk@sigma.ug.edu.pl>
2013-11-12 19:58:48 +0100

    program oblicza pole deltoidu

---
+++ dom/laba3/2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+ main(){
+ int p,q,pole ;
+printf("program liczy pole detoidu\n");
+printf("podaj p:");scanf("%d",&p);
+printf("podaj q:");scanf("%d",&q);
+pole=p*q/2;
+printf("pole deltoidu wynosi:%d\n",pole);
+
+return 0;
+}
+
